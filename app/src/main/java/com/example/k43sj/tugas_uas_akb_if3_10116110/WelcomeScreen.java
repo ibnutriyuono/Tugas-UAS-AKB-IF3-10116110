45676958b6a42170120207c5fd24c05e663a6732
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-10 18:59:45 +0700

    initial commit welcome screen

---
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/WelcomeScreen.java
@@ -0,0 +1,20 @@
+package com.example.k43sj.tugas_uas_akb_if3_10116110;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+ * 10 Agustus 2019
+ * 10116110
+ * Muhamad Ibnu Tri Yuono
+ * IF-3
+ */
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
