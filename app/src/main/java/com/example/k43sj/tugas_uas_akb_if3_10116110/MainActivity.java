11aba1083af16d610426652a1a5dc5bcf0b11726
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-10 13:48:54 +0700

    initial commit

---
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.k43sj.tugas_uas_akb_if3_10116110;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/*
+ * 10 Agustus 2019
+ * 10116110
+ * Muhamad Ibnu Tri Yuono
+ * IF-3
+ */
+
+public class MainActivity extends AppCompatActivity{
+    private TextView myTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
