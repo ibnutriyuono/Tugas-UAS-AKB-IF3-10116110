4b66d5c2ed1da63494b13cf81fb6355ff7954316
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-10 14:17:24 +0700

    adding mahasiswa and user model

--- app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/Mahasiswa.java
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/Mahasiswa.java
@@ -8,4 +8,73 @@ package com.example.k43sj.tugas_uas_akb_if3_10116110.model;
  */
 
 public class Mahasiswa {
+    private int image;
+    private String nim,nama,kelas,telephone,email,instagram;
+
+    public Mahasiswa(int image, String nim, String nama, String kelas, String telephone, String email, String instagram){
+        this.image = image;
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.telephone = telephone;
+        this.email = email;
+        this.instagram = instagram;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
 }
--- app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/User.java
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/User.java
@@ -8,4 +8,26 @@ package com.example.k43sj.tugas_uas_akb_if3_10116110.model;
  */
 
 public class User {
+    private String username, password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
