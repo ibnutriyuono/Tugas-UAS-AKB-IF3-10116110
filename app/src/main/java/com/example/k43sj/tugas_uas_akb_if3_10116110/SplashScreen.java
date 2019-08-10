1512edfae0617ff636a9719f02dd106b0b6e188a
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-10 16:47:35 +0700

    adding activity splash screen

---
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.k43sj.tugas_uas_akb_if3_10116110;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+ * 10 Agustus 2019
+ * 10116110
+ * Muhamad Ibnu Tri Yuono
+ * IF-3
+ */
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
