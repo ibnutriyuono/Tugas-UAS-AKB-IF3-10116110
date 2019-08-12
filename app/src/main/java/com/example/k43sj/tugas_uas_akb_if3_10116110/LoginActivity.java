96e760d51eb0663f22cdb446a6e789aa1282bc7c
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-12 11:38:15 +0700

    working login just need some validations at sharedpreferences

--- app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/LoginActivity.java
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.k43sj.tugas_uas_akb_if3_10116110;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.k43sj.tugas_uas_akb_if3_10116110.presenter.Login;
+import com.example.k43sj.tugas_uas_akb_if3_10116110.presenter.LoginPresenter;
+import com.example.k43sj.tugas_uas_akb_if3_10116110.view.LoginView;
 
 
 /*
@@ -17,20 +23,29 @@ import android.widget.Toast;
  * IF-3
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView {
     private TextView register;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
-    EditText textEditName, textEditPassword;
-    Button loginButton;
+    EditText textEditEmail, textEditPassword;
+    Button btnLogin;
 
+    LoginPresenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         register = (TextView) findViewById(R.id.link_signup);
-        textEditName = (EditText) findViewById(R.id.input_email);
-        textEditPassword  = (EditText) findViewById(R.id.input_password);
-        loginButton = (Button) findViewById(R.id.btn_login);
+        textEditEmail = (EditText) findViewById(R.id.input_email);
+        textEditPassword = (EditText) findViewById(R.id.input_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        presenter = new Login(this);
+        sharedPreferences = getSharedPreferences("Login", Context.MODE_PRIVATE);
+        final String email = sharedPreferences.getString("email", null);
+        final String password = sharedPreferences.getString("password", null);
+        textEditEmail.setText(email);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,12 +55,26 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent i = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(i);
-                finish();
+                String strEmail = textEditEmail.getText().toString();
+                String strPassword = textEditPassword.getText().toString();
+                presenter.onLogin(strEmail,strPassword,email,password);
             }
         });
+    }
+
+    @Override
+    public void onSuccess() {
+        Toast.makeText(this, "Berhasil Login", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public void onError(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/User.java
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/User.java
@@ -8,6 +8,7 @@ package com.example.k43sj.tugas_uas_akb_if3_10116110.model;
  */
 
 import android.text.TextUtils;
+import android.util.Patterns;
 
 import com.example.k43sj.tugas_uas_akb_if3_10116110.presenter.LoginPresenter;
 
@@ -18,16 +19,19 @@ public class User {
         this.presenter=presenter;
     }
 
-    public void validateUser(String username, String password , String dataUsername, String dataPassword) {
+    public void validateUser(String email, String password , String dataUsername, String dataPassword) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-        if(TextUtils.isEmpty(username))
+        if(TextUtils.isEmpty(email))
             presenter.onError("Please Enter Username !");
         else if(TextUtils.isEmpty(password))
             presenter.onError("Please Enter Password");
-        else if(!dataUsername.equals(username))
-            presenter.onError("Wrong Username !");
-        else if(!dataPassword.equals(password))
-            presenter.onError("Wrong Password !");
+        else if(!email.trim().matches(emailPattern))
+            presenter.onError("Please enter valid email");
+//        else if(!dataUsername.equals(email))
+//            presenter.onError("Wrong Username !");
+//        else if(!dataPassword.equals(password))
+//            presenter.onError("Wrong Password !");
         else
             presenter.onSuccess();
     }
