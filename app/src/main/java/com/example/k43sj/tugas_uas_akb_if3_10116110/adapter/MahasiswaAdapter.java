fc4e4a03cb3585e1d70b3e1a50bd8654c7fecab9
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-12 14:58:42 +0700

    adding creds

--- app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/adapter/MahasiswaAdapter.java
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/adapter/MahasiswaAdapter.java
@@ -1,4 +1,10 @@
 package com.example.k43sj.tugas_uas_akb_if3_10116110.adapter;
 
+/*
+ * 12 Agustus 2019
+ * 10116110
+ * Muhamad Ibnu Tri Yuono
+ * IF-3
+ */
 public class MahasiswaAdapter {
 }
--- app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/helper/RealmHelper.java
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/helper/RealmHelper.java
@@ -1,4 +1,11 @@
 package com.example.k43sj.tugas_uas_akb_if3_10116110.helper;
 
+/*
+ * 12 Agustus 2019
+ * 10116110
+ * Muhamad Ibnu Tri Yuono
+ * IF-3
+ */
+
 public class RealmHelper {
 }
