e4b80bcdb636955a36304ea340d4303edb2b6ba3
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-12 14:58:02 +0700

    initial commit

---
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/adapter/MahasiswaAdapter.java
@@ -0,0 +1,4 @@
+package com.example.k43sj.tugas_uas_akb_if3_10116110.adapter;
+
+public class MahasiswaAdapter {
+}
---
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/helper/RealmHelper.java
@@ -0,0 +1,4 @@
+package com.example.k43sj.tugas_uas_akb_if3_10116110.helper;
+
+public class RealmHelper {
+}
