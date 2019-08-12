bd60248f9d09506ea6168a747ea144be61f2f352
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-12 08:46:21 +0700

    adding validate method

--- app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/User.java
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/model/User.java
@@ -7,13 +7,28 @@ package com.example.k43sj.tugas_uas_akb_if3_10116110.model;
  * IF-3
  */
 
+import android.text.TextUtils;
+
 import com.example.k43sj.tugas_uas_akb_if3_10116110.presenter.LoginPresenter;
 
 public class User {
-    private String username, password;
     private LoginPresenter presenter;
 
     public User(LoginPresenter presenter){
         this.presenter=presenter;
+    }
+
+    public void validateUser(String username, String password , String dataUsername, String dataPassword) {
+
+        if(TextUtils.isEmpty(username))
+            presenter.onError("Please Enter Username !");
+        else if(TextUtils.isEmpty(password))
+            presenter.onError("Please Enter Password");
+        else if(!dataUsername.equals(username))
+            presenter.onError("Wrong Username !");
+        else if(!dataPassword.equals(password))
+            presenter.onError("Wrong Password !");
+        else
+            presenter.onSuccess();
     }
 }
