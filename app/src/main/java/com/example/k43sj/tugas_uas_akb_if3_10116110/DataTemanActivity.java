b42e2fcf5b5997959af84521cf7336bf65a64623
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-08-12 14:49:34 +0700

    initial commit

---
+++ app/src/main/java/com/example/k43sj/tugas_uas_akb_if3_10116110/DataTemanActivity.java
@@ -0,0 +1,13 @@
+package com.example.k43sj.tugas_uas_akb_if3_10116110;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataTemanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_teman);
+    }
+}
